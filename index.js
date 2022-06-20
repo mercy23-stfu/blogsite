90599892ed646b6748df4ecb88624f2b27b86ac1
Mercy Aremu <mercyaremu3@gmail.com>
2022-06-20 19:32:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// grab elements
+
+// nav styles on scroll
+
+// open menu and search pop-up
+
+// open/close serch form popup
+
+// --close the search form popup on esc keypress
+
+// switch theme/add to local storage
+
+// swiper
